b72d7a1a3f74d9c36ca860edc32b9facd68d6932
Quincy Michels <quincy.michels@gmail.com>
2025-02-12 11:19:39 -0500

    added shop script (no functionality). Made countdown use the players framerate to look smoother on high-end pc's

---
+++ Source/BeersAndBrawls/Actors/Shop.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Shop.h"
+
+// Sets default values
+AShop::AShop()
+{
+ 	
+}
+
+// Called when the game starts or when spawned
+void AShop::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
