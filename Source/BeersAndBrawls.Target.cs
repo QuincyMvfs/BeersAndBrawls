661a2e5a06e053ba7d3fce1152237081f991441f
Quincy Michels <quincy.michels@gmail.com>
2026-03-30 15:06:04 -0400

    updated engine version to 5.7.4

--- Source/BeersAndBrawls.Target.cs
+++ Source/BeersAndBrawls.Target.cs
@@ -8,8 +8,8 @@ public class BeersAndBrawlsTarget : TargetRules
 	public BeersAndBrawlsTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("BeersAndBrawls");
 	}
 }
