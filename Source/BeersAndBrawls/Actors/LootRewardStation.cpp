3c5459cbb8154a8c9798d738b8e236ff98fe753d
Quincy Michels <quincy.michels@gmail.com>
2026-04-01 19:47:21 -0400

    added LootRewardStation script

---
+++ Source/BeersAndBrawls/Actors/LootRewardStation.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LootRewardStation.h"
+
+// Sets default values
+ALootRewardStation::ALootRewardStation()
+{
+	
+}
+
+// Called when the game starts or when spawned
+void ALootRewardStation::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
